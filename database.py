be3597c4bb34db197fef0a99e99ac82b87383b18
nk1926 <nk1926907@gmail.com>
2026-05-16 21:58:01 +0500

    updates

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/attendance"
-
+DATABASE_URL = "mysql+pymysql://admin:secret@mysql:3306/attendance"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
